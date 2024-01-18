f52e7a01fdabf02c8ccd86277e9da59a47cf71d9
xlorne <1991wangliang@gmail.com>
2024-01-18 16:20:36 +0800

    flowchart

--- flow-design/src/flow/index.tsx
+++ flow-design/src/flow/index.tsx
@@ -1,217 +1,98 @@
-import React, {useEffect, useRef} from 'react';
-import {Graph} from '@antv/x6';
-import './index.less';
+import React from 'react';
+import {Flowchart} from '@ant-design/flowchart';
+import "@ant-design/flowchart/dist/index.css";
+import "antd/dist/reset.css";
 
-Graph.registerNode(
-    'custom-node',
-    {
-        markup: [
-            {
-                tagName: 'rect',
-                selector: 'body',
-            },
-            {
-                tagName: 'text',
-                selector: 'label',
-            },
-            {
-                tagName: 'g',
-                children: [
-                    {
-                        tagName: 'text',
-                        selector: 'btnText',
-                    },
-                    {
-                        tagName: 'rect',
-                        selector: 'btn',
-                    },
-                ],
-            },
-        ],
-        attrs: {
-            btn: {
-                refX: '100%',
-                refX2: -28,
-                y: 4,
-                width: 24,
-                height: 18,
-                rx: 10,
-                ry: 10,
-                fill: 'rgba(255,255,0,0.01)',
-                stroke: 'red',
-                cursor: 'pointer',
-                event: 'node:delete',
-            },
-            btnText: {
-                fontSize: 14,
-                fill: 'red',
-                text: 'x',
-                refX: '100%',
-                refX2: -19,
-                y: 17,
-                cursor: 'pointer',
-                pointerEvent: 'none',
-            },
-            body: {
-                stroke: '#8f8f8f',
-                strokeWidth: 1,
-                fill: '#fff',
-                rx: 6,
-                ry: 6,
-                refWidth: '100%',
-                refHeight: '100%',
-            },
-            label: {
-                fontSize: 14,
-                fill: '#333333',
-                refX: '50%',
-                refY: '50%',
-                textAnchor: 'middle',
-                textVerticalAnchor: 'middle',
-            },
-        },
-        ports: {
-            groups: {
-                top: {
-                    position: 'top',
-                    attrs: {
-                        circle: {
-                            r: 4,
-                            magnet: true,
-                            stroke: '#31d0c6',
-                            strokeWidth: 2,
-                            fill: '#fff',
-                        },
-                    },
-                },
-                right: {
-                    position: 'right',
-                    attrs: {
-                        circle: {
-                            r: 4,
-                            magnet: true,
-                            stroke: '#31d0c6',
-                            strokeWidth: 2,
-                            fill: '#fff',
-                        },
-                    },
-                },
-                bottom: {
-                    position: 'bottom',
-                    attrs: {
-                        circle: {
-                            r: 4,
-                            magnet: true,
-                            stroke: '#31d0c6',
-                            strokeWidth: 2,
-                            fill: '#fff',
-                        },
-                    },
-                },
-                left: {
-                    position: 'left',
-                    attrs: {
-                        circle: {
-                            r: 4,
-                            magnet: true,
-                            stroke: '#31d0c6',
-                            strokeWidth: 2,
-                            fill: '#fff',
-                        },
-                    },
-                },
-            },
-            items: [
-                {
-                    id: 'port1',
-                    group: 'top',
-                },
-                {
-                    id: 'port2',
-                    group: 'right',
-                },
-                {
-                    id: 'port3',
-                    group: 'bottom',
-                },
-                {
-                    id: 'port4',
-                    group: 'left',
-                },
-            ],
-        }
-    },
-    true,
-)
-export const Flow = () => {
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const graph = new Graph({
-            //@ts-ignore
-            container: containerRef?.current,
-            width: 800,
-            height: 800,
-            background: {
-                color: '#fffbe6',
-            },
-            connecting: {
-                snap: true,
-            },
-            grid: {
-                size: 10,      // 网格大小 10px
-                visible: true, // 渲染网格背景
-            },
-        });
-
-        //@ts-ignore
-        graph.on('node:delete', ({view, e}) => {
-            e.stopPropagation()
-            view.cell.remove()
-        })
-
-        const source = graph.addNode({
-            shape: 'custom-node',
-            x: 40,
-            y: 40,
-            width: 120,
-            height: 40,
-            attrs: {
-                label: {
-                    text: 'Source',
-                },
-            },
-        })
-
-        const target = graph.addNode({
-            shape: 'custom-node',
-            x: 160,
-            y: 240,
-            width: 120,
-            height: 40,
-            attrs: {
-                label: {
-                    text: 'Target',
-                },
-            },
-        })
-
-        // graph.centerContent();
-        // graph.fromJSON(data);
-        //@ts-ignore
-        graph.on('node:delete', ({view, e}) => {
-            e.stopPropagation()
-            view.cell.remove()
-        })
-
-
-    }, []);
-
+const IndicatorNode = (props:any) => {
+    const { size = { width: 120, height: 50 }, data } = props;
+    const { width, height } = size;
+    const { label = '自定义节点', stroke = '#ccc', fill = '#fff', fontFill, fontSize } = data;
 
     return (
-        <div className="react-shape-app">
-            <div className="app-content" ref={containerRef}/>
+        <div
+            className="indicator-container"
+            style={{
+                position: 'relative',
+                display: 'block',
+                background: '#fff',
+                border: '1px solid #84b2e8',
+                borderRadius: '2px',
+                padding: '10px 12px',
+                overflow: 'hidden',
+                boxShadow: '0 1px 4px 0 rgba(0,0,0,0.20)',
+                width,
+                height,
+                borderColor: stroke,
+                backgroundColor: fill,
+                color: fontFill,
+                fontSize,
+            }}
+        >
+            <div style={{ color: fontFill }}>{label}</div>
         </div>
     );
 };
 
+export const Flow = () => {
+    return (
+        <div style={{ height: 600 }}>
+            <Flowchart
+                onSave={(d) => {
+                    console.log(d);
+                }}
+                toolbarPanelProps={{
+                    position: {
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                    },
+                }}
+                scaleToolbarPanelProps={{
+                    layout: 'horizontal',
+                    position: {
+                        right: 0,
+                        top: -40,
+                    },
+                    style: {
+                        width: 150,
+                        height: 39,
+                        left: 'auto',
+                        background: 'transparent',
+                    },
+                }}
+                canvasProps={{
+                    position: {
+                        top: 40,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                    },
+                }}
+                nodePanelProps={{
+                    //隐藏通用节点
+                    showOfficial:false,
+                    position: { width: 160, top: 40, bottom: 0, left: 0 },
+                    defaultActiveKey: ['custom'], // ['custom', 'official']
+                    //@ts-ignore
+                    registerNode: {
+                        title: '指标节点',
+                        key: 'custom',
+                        nodes: [
+                            {
+                                component: IndicatorNode,
+                                popover: () => <div>指标节点</div>,
+                                name: 'custom-node-indicator',
+                                width: 120,
+                                height: 50,
+                                label: '自定义节点',
+                            },
+                        ],
+                    },
+                }}
+                detailPanelProps={{
+                    position: { width: 200, top: 40, bottom: 0, right: 0 },
+                }}
+            />
+        </div>
+    );
+};
