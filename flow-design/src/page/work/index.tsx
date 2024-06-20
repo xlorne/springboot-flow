c4b766865338ff322bb11e937742a7ce18ac8c04
lorne <1991wangliang@gmail.com>
2024-06-20 10:00:52 +0800

    add layout

--- flow-design/src/page/work/index.tsx
+++ flow-design/src/page/work/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {PageContainer, ProTable} from "@ant-design/pro-components";
 import {todo} from "@/api/work";
+import {Button} from "antd";
 
 const WorkPage = () => {
 
@@ -12,11 +13,18 @@ const WorkPage = () => {
         },
         {
             dataIndex: 'name',
-            title: '账户',
+            title: '节点名称',
         },
         {
-            dataIndex: 'password',
-            title: '密码',
+            dataIndex: 'node',
+            title: '下一节点',
+            search: false,
+            render: (text: any,recode:any) => {
+                if(recode.node){
+                    return recode.node.name;
+                }
+                return '-';
+            }
         },
         {
             dataIndex: 'createTime',
@@ -41,6 +49,19 @@ const WorkPage = () => {
             title: '审批状态',
             search: false,
         },
+        {
+            dataIndex: 'option',
+            title: '操作',
+            valueType: 'option',
+            render: (text: any,recode:any) => {
+                return [
+                    <a key="pass">通过</a>,
+                    <a key="reject">拒绝</a>,
+                    <a key="recall">召回</a>,
+                    <a key="back">退回</a>,
+                ]
+            }
+        },
 
     ]
 
@@ -50,6 +71,12 @@ const WorkPage = () => {
             <ProTable
                 rowKey={"id"}
                 columns={columns}
+                toolBarRender={() => {
+                    return [
+                        <Button type={'primary'} onClick={()=>{
+                        }}>发起请假</Button>
+                    ]
+                }}
                 request={async (params,sort,filter) => {
                     return todo(params,sort,filter,[]);
                 }}
